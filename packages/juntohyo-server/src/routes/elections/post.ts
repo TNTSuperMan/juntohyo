10410e1f1f1dbb10a37cf74ee5ad5e2a98783aca
TNTSuperMan <tnt-super-man@outlook.com>
2025-10-18 16:19:03 +0900

    POST /electionsのスキーマを作る

--- packages/juntohyo-server/src/routes/elections/post.ts
+++ packages/juntohyo-server/src/routes/elections/post.ts
@@ -1,4 +1,11 @@
+import z from "zod";
 import { app } from "../../app";
+
+const postElectionsBodySchema = z.object({
+    title: z.string().max(128),
+    description: z.string().max(1024),
+    options: z.array(z.string().max(128)).max(16),
+})
 
 app.post("/elections", (c) => {
     return c.text("501 Not Implemented", 501);
