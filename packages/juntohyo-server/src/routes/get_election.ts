cb76f2107e502d80953a4068ea845ebc716e6dce
TNTSuperMan <tnt-super-man@outlook.com>
2025-11-21 13:50:46 +0900

    投票箱情報を取得するAPI追加

---
+++ packages/juntohyo-server/src/routes/get_election.ts
@@ -0,0 +1,16 @@
+import { app } from "../app";
+import type { Election } from "../types";
+import { ClientError, ErrorCodes } from "../utils/client_error";
+
+app.get("/elections/:id", async (c) => {
+    const raw_election = await c.env.ELECTIONS_KV.get(c.req.param("id"));
+    if(!raw_election) {
+        throw new ClientError(ErrorCodes.IncorrectRequest);
+    }
+    const election = JSON.parse(raw_election) as Election;
+    return c.json({
+        title: election.title,
+        description: election.description,
+        options: election.options,
+    });
+});
--- packages/juntohyo-server/src/routes/index.ts
+++ packages/juntohyo-server/src/routes/index.ts
@@ -1,3 +1,4 @@
 import "./create_election";
 import "./delete_election";
+import "./get_election";
 import "./vote";
