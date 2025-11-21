5d128b1e67b9008e3d522dd2acc1c0b6716bd0ea
TNTSuperMan <tnt-super-man@outlook.com>
2025-11-21 14:26:15 +0900

    投票取得API追加(サーバーは一通り完了)

---
+++ packages/juntohyo-server/src/routes/get_votes.ts
@@ -0,0 +1,16 @@
+import { app } from "../app";
+import { verifyToken } from "../utils/authentication";
+
+app.get("/elections/:id/votes", async (c) => {
+    const id = c.req.param("id");
+    await verifyToken(c, c.req.header("Authorization") ?? "", id);
+
+    const query_result = await c.env.VOTES_DB
+        .prepare("SELECT choice, COUNT(*) AS count FROM votes WHERE election = ? GROUP BY choice ORDER BY choice;")
+        .bind(Buffer.from(id, "base64url"))
+        .all();
+    
+    const response = Object.fromEntries(query_result.results.map(e => [e.choice, e.count]));
+
+    return c.json(response);
+});
--- packages/juntohyo-server/src/routes/index.ts
+++ packages/juntohyo-server/src/routes/index.ts
@@ -1,4 +1,5 @@
 import "./create_election";
 import "./delete_election";
 import "./get_election";
+import "./get_votes";
 import "./vote";
