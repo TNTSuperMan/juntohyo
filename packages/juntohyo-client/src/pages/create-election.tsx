1ef7be42e865818344161937b7615b8912fb99f3
TNTSuperMan <tnt-super-man@outlook.com>
2025-11-23 13:25:12 +0900

    投票箱作成ページ(途中)

---
+++ packages/juntohyo-client/src/pages/create-election.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+export function CreateElection() {
+    const [title, setTitle] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+
+    return <main>
+        <h2>投票箱を作る</h2>
+        <label>
+            タイトル:
+            <input type="text" name="title" maxLength={128} value={title} onChange={e=>setTitle(e.target.value)} />
+        </label>
+        <br />
+        <label>
+            説明: <br />
+            <textarea name="description" maxLength={1024} value={description} onChange={e=>setDescription(e.target.value)} />
+        </label>
+    </main>
+}
--- packages/juntohyo-client/src/router.tsx
+++ packages/juntohyo-client/src/router.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Index } from "./pages";
+import { CreateElection } from "./pages/create-election";
 
 export function Router() {
     return <BrowserRouter>
         <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/create-election" element={<CreateElection />} />
         </Routes>
     </BrowserRouter>
 }
