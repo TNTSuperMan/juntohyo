0c69efd49b8ae29bdfcd5f249126804aace92257
TNTSuperMan <tnt-super-man@outlook.com>
2025-11-23 12:31:45 +0900

    CSSをちょっと

--- packages/juntohyo-client/src/index.tsx
+++ packages/juntohyo-client/src/index.tsx
@@ -3,6 +3,12 @@ import { StrictMode } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Index } from "./pages";
 
+import "./style/normal.css";
+
+document.body.classList.add(
+    localStorage.getItem("juntohyo-theme") ?? "normal"
+);
+
 createRoot(document.body).render(<StrictMode>
     <BrowserRouter>
         <Routes>
