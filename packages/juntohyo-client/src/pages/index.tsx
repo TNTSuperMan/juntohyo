49d93236af2e8e5f5461dc05ebbc3895d017c2db
TNTSuperMan <tnt-super-man@outlook.com>
2025-11-23 13:01:33 +0900

    スタイリング

--- packages/juntohyo-client/src/pages/index.tsx
+++ packages/juntohyo-client/src/pages/index.tsx
@@ -2,13 +2,10 @@ import { Link } from "react-router-dom";
 
 export function Index() {
     return <main>
-        <p>簡単に使える投票アプリ。</p>
-        <ul className="inline">
-            <li>登録不要</li>
-            <li>シンプル</li>
-            <li>無料</li>
-        </ul>
-        <p>投票箱を作ってみましょう。</p>
-        <Link className="button" to="/create-election">投票箱を作成</Link>
+        <p>
+            オープンソース、無料、登録不要、シンプル。<br />
+            簡単に使える投票アプリ。
+        </p>
+        <Link className="create-button" to="/create-election">投票箱を作成</Link>
     </main>
 }
