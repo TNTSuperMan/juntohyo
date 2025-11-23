b47153126e2ff2c3af9e220f4b8e5e03cb220d2e
TNTSuperMan <tnt-super-man@outlook.com>
2025-11-23 12:12:06 +0900

    金型を変える

--- packages/juntohyo-client/src/index.tsx
+++ packages/juntohyo-client/src/index.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
 import { StrictMode } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Index } from "./pages";
 
 createRoot(document.body).render(<StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Index />} />
+        </Routes>
+    </BrowserRouter>
 </StrictMode>);
---
+++ packages/juntohyo-client/src/pages/index.tsx
@@ -0,0 +1,5 @@
+export function Index() {
+    return <div className="root">
+        <h1>純投票</h1>
+    </div>
+}
