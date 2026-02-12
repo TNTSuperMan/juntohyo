e1a20f2319b241965bb4808ca2a9f5d025c3a765
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-02-12 11:46:39 +0900

    投票画面(仮)

---
+++ packages/juntohyo-client/src/pages/vote.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { server } from "../server";
+import { ErrorCodes, type ErrorResponse } from "juntohyo-abstract-server/src/utils/error";
+import type { Option } from "juntohyo-abstract-server/src/types";
+import { ErrorView } from "../components/Error";
+
+export function Vote() {
+    const { id } = useParams() as { id: string };
+    const [election, setElection] = useState<{
+        title: string, description: string, options: Option[],
+    }>();
+    const [error, setError] = useState<ErrorResponse>();
+
+    useEffect(() => {
+        server.elections[":id"].$get({
+            param: { id },
+        }).then(async election_response => {
+            const res_json = await election_response.json().catch(() => ({ code: ErrorCodes.ServerError }));
+            if ("code" in res_json) {
+                setError(res_json);
+            } else {
+                setElection(res_json);
+            }
+        });
+    }, [id]);
+
+    if (election) {
+        return <main className="vote">
+            <h1>投票: {election.title}</h1>
+            <pre className="description">
+                { election.description }
+            </pre>
+        </main>
+    } else {
+        return <main className="vote">
+            { error ? <ErrorView error={error} /> : "読込中..." }
+        </main>
+    }
+}
--- packages/juntohyo-client/src/router.tsx
+++ packages/juntohyo-client/src/router.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Index } from "./pages";
 import { CreateElection } from "./pages/create-election";
+import { Vote } from "./pages/vote";
 
 export function Router() {
     return <BrowserRouter>
         <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/create-election" element={<CreateElection />} />
+            <Route path="/vote/:id" element={<Vote />} />
         </Routes>
     </BrowserRouter>
 }
